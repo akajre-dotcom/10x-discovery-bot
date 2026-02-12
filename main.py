332fabd9a44a2a4c734bd3c4da7a6ff62937419a
akajre-dotcom <akajre@gmail.com>
2026-02-12 16:48:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from scraper import get_screen_data
+from scraper import get_all_data
+from ranking import rank_stocks
 from ai_analysis import generate_thesis
 from emailer import send_email
 
-stocks = get_screen_data()
+data = get_all_data()
+top_stocks = rank_stocks(data)
 
-report = "WEEKLY 10X DISCOVERY REPORT\n\n"
+report = "WEEKLY STOCK DISCOVERY REPORT\n\n"
 
-for stock in stocks:
-    thesis = generate_thesis(stock)
-    report += "\n----------------------\n"
+for _, row in top_stocks.iterrows():
+    report += f"\nBucket: {row['Bucket']}\n"
+    report += f"Stock: {row['Name']}\n"
+    report += f"Sales Growth: {row['SalesGrowth']}%\n"
+    report += f"ROCE: {row['ROCE']}%\n"
+    report += f"Score: {round(row['Score'],2)}\n"
+
+    thesis = generate_thesis(row.to_dict())
     report += thesis
+    report += "\n-----------------------------\n"
 
 send_email(report)
