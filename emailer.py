9e8cf04f29517e9f21b6fe1ecf177589f96b9ba8
akajre-dotcom <akajre@gmail.com>
2026-02-12 23:52:53 +0530

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -1,16 +1,18 @@
-import os
 import smtplib
+import os
 from email.mime.text import MIMEText
 
-def send_email(content):
+def send_email(body, subject):
+
     sender = os.getenv("EMAIL_ADDRESS")
     password = os.getenv("EMAIL_PASSWORD")
+    receiver = os.getenv("EMAIL_ADDRESS")
 
-    msg = MIMEText(content)
-    msg["Subject"] = "Weekly 10x Discovery Report"
+    msg = MIMEText(body)
+    msg["Subject"] = subject
     msg["From"] = sender
-    msg["To"] = sender
+    msg["To"] = receiver
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(sender, password)
-        server.sendmail(sender, sender, msg.as_string())
+        server.sendmail(sender, receiver, msg.as_string())
