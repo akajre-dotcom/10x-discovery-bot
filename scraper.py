c1a54b75fa0d1069e5e6d1c1208833f006970429
akajre-dotcom <akajre@gmail.com>
2026-02-12 17:11:53 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -39,6 +39,7 @@ def fetch_screen(screen_name, base_url):
                     "ProfitGrowth": extract_number(cols[3]),
                     "ROCE": extract_number(cols[4]),
                     "Bucket": screen_name
+                    "MarketCap": extract_number(cols[1]),
                 })
 
         page += 1
